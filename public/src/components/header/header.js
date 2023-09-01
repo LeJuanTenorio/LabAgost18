395bc1269e7476c3c85ba1a4cd03d697f5c493de
Juan Camilo Tenorio Munoz <1193225773@u.icesi.edu.co>
2023-09-01 12:17:52 -0500

    Commit 2

--- public/src/components/header/header.js
+++ public/src/components/header/header.js
@@ -1,4 +1,4 @@
-class Header extends HTMLElement{
+class HeaderOne extends HTMLElement{
 
     constructor(){
         super();
@@ -8,4 +8,17 @@ class Header extends HTMLElement{
     connectedCallback(){
         this.render();
     }
+
+    render(){
+        if (this.shadowRoot){
+            this.shadowRoot.innerHTML = `
+            <link rel="stylesheet" href"./src/components/header/header.css>
+            <section>
+            <h1>${this.name}</h1>
+            </section>`;
+        }
+    }
 }
+
+customElements.define(`header-one`, HeaderOne);
+export default HeaderOne; 
---
+++ public/src/components/indexHijo.js
@@ -0,0 +1,1 @@
+export {default as HeaderOne} from "./header/header.js"
